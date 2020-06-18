9959ed21c639217e8336d801c2850c601d097ea6
Juan padilla Julian <jtpadilla@gmail.com>
2020-06-18 13:16:13 +0200

    Estudio

---
+++ main/mainFreeRtos.cpp
@@ -0,0 +1,49 @@
+
+#include <stdio.h>
+#include "freertos/Task.h"
+#include "system/GeneralUtils.h"
+
+class MyTask: public freertos::Task
+{
+
+	public:
+
+		MyTask(std::string taskName, uint32_t millisecsParam):
+			freertos::Task{taskName}, millisecs {millisecsParam}
+		{
+		}
+
+		void run(void* data) {
+
+			int count = 0;
+
+			while(count++ < 10) {
+				printf("[%s] count: %d\n", m_taskName.c_str(), count);
+				delay(millisecs);
+			}
+
+			systemm::GeneralUtils::dumpInfo();
+			printf("Done\n");
+
+		}
+
+	private:
+		uint32_t millisecs;
+
+};
+
+void mainFreeRtos() {
+
+	printf("Inicio TTN!\n");
+
+	MyTask* pMyTask1 = new MyTask("uno", 1000);
+	pMyTask1->setStackSize(20000);
+	pMyTask1->start();
+
+	MyTask* pMyTask2 = new MyTask("dossssssss", 2000);
+	pMyTask2->setStackSize(20000);
+	pMyTask2->start();
+
+	printf("Final de la tarea principal!");
+
+}
--- main/mainTtn.cpp
+++ main/mainTtn.cpp
@@ -10,7 +10,7 @@ constexpr char devEui[] = "004CFEED74AD2FA6";
 constexpr char appEui[] = "70B3D57ED00306F7";
 constexpr char appKey[] = "8214F6A2800C9FCD9B26BBE28D5CD057";
 
-extern "C" void app_main(void)
+void mainTtn(void)
 {
 
     // Se prepara la configuracion para conectar con TTN
