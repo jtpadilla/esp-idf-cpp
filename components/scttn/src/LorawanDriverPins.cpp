136b0156468bb11140c0e2557aaaa2b13a5e145a
Juan Padilla <padilla@speedycontrol.com>
2020-06-20 20:15:46 +0200

    Estudio

--- components/scttn/src/LorawanDriverPins.cpp
+++ components/scttn/src/LorawanDriverPins.cpp
@@ -8,44 +8,44 @@ namespace genielink::lorawan
 {
 
     LorawanDriverPins::LorawanDriverPins(
-                spi_host_device_t spi_host_arg,
-                uint8_t spi_dma_chan_arg,
-                uint8_t pin_spi_sclk_arg,
-                uint8_t pin_spi_mosi_arg,
-                uint8_t pin_spi_miso_arg,
-                uint8_t pin_nss_arg,
-                uint8_t pin_rxtx_arg,
-                uint8_t pin_rst_arg,
-                uint8_t pin_di00_arg,
-                uint8_t pin_di01_arg):
-                spi_host {spi_host_arg},
-                spi_dma_chan {spi_dma_chan_arg},
-                pin_spi_sclk {pin_spi_sclk_arg},
-                pin_spi_mosi {pin_spi_mosi_arg},
-                pin_spi_miso {pin_spi_miso_arg},
-                pin_nss {pin_nss_arg},
-                pin_rxtx {pin_rxtx_arg},
-                pin_rst {pin_rst_arg},
-                pin_di00 {pin_di00_arg},
-                pin_di01 {pin_di01_arg}
+        spi_host_device_t spi_host_arg,
+        uint8_t spi_dma_chan_arg,
+        uint8_t pin_spi_sclk_arg,
+        uint8_t pin_spi_mosi_arg,
+        uint8_t pin_spi_miso_arg,
+        uint8_t pin_nss_arg,
+        uint8_t pin_rxtx_arg,
+        uint8_t pin_rst_arg,
+        uint8_t pin_di00_arg,
+        uint8_t pin_di01_arg):
+        spi_host {spi_host_arg},
+        spi_dma_chan {spi_dma_chan_arg},
+        pin_spi_sclk {pin_spi_sclk_arg},
+        pin_spi_mosi {pin_spi_mosi_arg},
+        pin_spi_miso {pin_spi_miso_arg},
+        pin_nss {pin_nss_arg},
+        pin_rxtx {pin_rxtx_arg},
+        pin_rst {pin_rst_arg},
+        pin_di00 {pin_di00_arg},
+        pin_di01 {pin_di01_arg}
     {
 
-                esp_err_t err;
-                
-                // Initialize the GPIO ISR handler service
-                err = gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
-                ESP_ERROR_CHECK(err);
+        esp_err_t err;
+        
+        // Initialize the GPIO ISR handler service
+        err = gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
+        ESP_ERROR_CHECK(err);
 
-                // Initialize SPI bus
-                spi_bus_config_t spi_bus_config{};
-                spi_bus_config.miso_io_num = pin_spi_miso;
-                spi_bus_config.mosi_io_num = pin_spi_mosi;
-                spi_bus_config.sclk_io_num = pin_spi_sclk;
-                spi_bus_config.quadwp_io_num = -1;
-                spi_bus_config.quadhd_io_num = -1;
-                spi_bus_config.max_transfer_sz = 0;
-                err = spi_bus_initialize(spi_host, &spi_bus_config, spi_dma_chan);
-                ESP_ERROR_CHECK(err);
+        // Initialize SPI bus
+        spi_bus_config_t spi_bus_config{};
+        spi_bus_config.miso_io_num = pin_spi_miso;
+        spi_bus_config.mosi_io_num = pin_spi_mosi;
+        spi_bus_config.sclk_io_num = pin_spi_sclk;
+        spi_bus_config.quadwp_io_num = -1;
+        spi_bus_config.quadhd_io_num = -1;
+        spi_bus_config.max_transfer_sz = 0;
+        err = spi_bus_initialize(spi_host, &spi_bus_config, spi_dma_chan);
+        ESP_ERROR_CHECK(err);
 
     }
 
@@ -88,6 +88,5 @@ namespace genielink::lorawan
     uint8_t LorawanDriverPins::get_pin_di01() const {
         return pin_di01;
     }
-
 
 }
